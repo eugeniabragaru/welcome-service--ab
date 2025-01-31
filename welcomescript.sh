8503732bd482ff08c96dcdff1c42a8874078cc5c
EC2 Default User <eugenia.bragaru@gmail.com>
2025-01-31 17:13:28 +0000

    Copied welcomescript.sh from welcome-service

--- welcomescript.sh
+++ welcomescript.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-echo "Hello 312 Stadent!"
-echo "Inter Your Name Please:"
-read name
-echo "$names, welcome to 313 School"
+
+# Display a welcome message
+echo "Welcome to the Welcome Service!"
+
+# Display the hostname of the system
+echo "This script is running on $(hostname)"
+
+# Display the current date and time
+echo "Current date and time: $(date)"
+
+# Display the user running the script
+echo "The script is being executed by user: $USER"
+
+exit 0
+
